20f1ad2a6eeb0938155f556d9e26b29ebb33e30f
timpengu <tim@peugniez.com>
2025-06-06 13:01:38 +0100

    Added GET route for OrientedPiece in PiecesController

--- GeniusSquare.WebAPI/Controllers/PiecesController.cs
+++ GeniusSquare.WebAPI/Controllers/PiecesController.cs
@@ -1,5 +1,7 @@
+using GeniusSquare.Core.Coords;
 using GeniusSquare.WebAPI.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GeniusSquare.WebAPI.Controllers;
 
@@ -33,5 +35,55 @@ public class PiecesController : ControllerBase
         }
 
         return piece;
+    }
+
+    [HttpGet("{pieceId}/{orientationId}")]
+    public ActionResult<Model.OrientedPiece> Get(string pieceId, string orientationId)
+    {
+        pieceId = pieceId.NormaliseId();
+        orientationId = orientationId.NormaliseId();
+
+        if (!_pieces.TryGetValue(pieceId, out Model.Piece? piece))
+        {
+            return NotFound($"Unknown piece: '{pieceId}'");
+        }
+
+        if (!TryGetOrientation(orientationId, out Orientation? orientation))
+        {
+            return NotFound($"Unknown orientation: '{orientationId}'");
+        }
+
+        List<Model.Coord> orientedPositions = piece.Positions
+            .Select(coord => (Coord)coord)
+            .Transform(orientation.Value)
+            .Normalise()
+            .Select(coord => (Model.Coord)coord)
+            .ToList();
+
+        return new Model.OrientedPiece
+        {
+            PieceId = pieceId,
+            OrientationId = orientationId,
+            Positions = orientedPositions
+        };
+    }
+
+    // TODO: Encapsulate orientation ID mapping in GeniusSquare.Core (see PieceBuilder)
+    private static bool TryGetOrientation(string orientationId, [NotNullWhen(true)] out Orientation? orientation)
+    {
+        orientation = orientationId switch
+        {
+            "a" or "ar" => Orientation.Ar,
+            "b" or "br" => Orientation.Br,
+            "c" or "cr" => Orientation.Cr,
+            "d" or "dr" => Orientation.Dr,
+            "al" => Orientation.Al,
+            "bl" => Orientation.Bl,
+            "cl" => Orientation.Cl,
+            "dl" => Orientation.Dl,
+            _ => null
+        };
+
+        return orientation != null;
     }
 }
